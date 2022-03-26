a207d98b76caf140e633bcf422737b08bfb890ad
saurabh <saurabh7481@gmail.com>
2022-03-26 15:52:05 +0530

    additional fixes

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,9 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
